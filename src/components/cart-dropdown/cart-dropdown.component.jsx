a75083433fbb0866b3b677a3322ec1fba0edaa47
unknown <hazartilirot@gmail.com>
2021-05-17 19:13:46 +0300

    wired up a memoization to cartItems so that it won't be rendered each time state updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component'
 
+import {connect} from 'react-redux'
+import CartItem from "../cart-item/cart-item.component";
+
+import {selectCartItems} from '../../redux/cart/cart.selectors'
+
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = (props) => {
+const CartDropdown = ({cartItems}) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {
+          cartItems.map(item => <CartItem key={item.id} item={item} />)
+        }
+      </div>
       <CustomButton>GO TO CHECK OUT</CustomButton>
     </div>
   );
 }
-export default CartDropdown;
+/*const mapStateToProps = ({ cart: {cartItems}}) => ({cartItems})*/
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
+import "./cart-icon.styles.scss";
 
-import './cart-icon.styles.scss'
-
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({ toggleCartHidden, counter }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{counter}</span>
     </div>
   );
-}
+};
+
+/*/!*The issue with this snippet is that it's executed (re-rendered) each time a
+state changes. The data needs to be memoized in memory so that it's rendered
+only when the data changes itself despite the state of an app. reselect*!/
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  counter: cartItems.reduce((acc, itm) => acc + itm.quantity, 0)  
+})*/
+
+const mapStateToProps = (state) => ({
+  counter: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
