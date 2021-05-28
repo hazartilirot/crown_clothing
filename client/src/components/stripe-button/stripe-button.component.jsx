185847dd10a58617112bc040021f0ac359ff401c
hazartilirot <hazartilirot@gmail.com>
2021-05-28 22:51:21 +0300

    implemented a backend part to charge Stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout'
 
+import axios from 'axios';
+
 const StripeCheckoutButton = ({price}) => {
   const priceInCents = price * 100;
   const publishableKey = process.env.REACT_APP_PUBLISHABLE_KEY;
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment successful')
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceInCents,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert('There was an issue with your payment')
+    })
   }
   
   return (
